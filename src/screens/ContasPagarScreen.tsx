aca735b6b70ea7c8b551301c8f7da7b020b24117
Heloisa <helonogalcabrera@gmail.com>
2026-03-19 22:11:50 -0300

    Telas Iniciais

---
+++ src/screens/ContasPagarScreen.tsx
@@ -0,0 +1,9 @@
+import { View, Text} from "react-native";
+
+export default function ContasPagarScreen(){
+return(
+    <View>
+        <Text>Contas a Pagar</Text>
+    </View>
+);
+}
---
+++ src/screens/ContasReceber.tsx
@@ -0,0 +1,9 @@
+import { View, Text} from "react-native";
+
+export default function ContasReceber(){
+return(
+    <View>
+        <Text>Contas a Receber</Text>
+    </View>
+);
+}
