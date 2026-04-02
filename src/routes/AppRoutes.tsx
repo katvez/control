732a04932281095d5739792906fef04de4cffcf0
Heloisa <helonogalcabrera@gmail.com>
2026-04-01 22:45:06 -0300

    01/04/2026

---
+++ src/routes/AppRoutes.tsx
@@ -0,0 +1,9 @@
+import AuthRoutes from "./AuthRoutes";
+import DrawerRoutes from "./DrawerRoutes";
+
+export default function AppRoutes() {
+
+    const isAuthenticated = false; 
+    return isAuthenticated ? <DrawerRoutes /> : <AuthRoutes />;
+
+}
---
+++ src/routes/AuthRoutes.tsx
@@ -0,0 +1,23 @@
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import LoginScreen from "../screens/LoginScreen";
+import RegisterScreen from "../screens/RegisterScreen";
+import AboutScreen from "../screens/AboutScreen";
+
+const Stack = createNativeStackNavigator();
+
+export default function AuthRoutes(){
+  return (
+    <Stack.Navigator>
+        
+      <Stack.Screen name="Login" component={LoginScreen} options={{ title:""}} />
+
+      <Stack.Screen name="Register" component={RegisterScreen} options={{ title:""}} />
+
+      <Stack.Screen name="About" component={AboutScreen} options={{ title:""}} />
+
+    </Stack.Navigator>
+  );
+};
+
+
